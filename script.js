04deadd97362ce45ece623e6e29e176418306e89
Simran Gulati <99637511+simrangulatii1002@users.noreply.github.com>
2022-07-17 13:26:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var check=false;
+
+    $('.clicks').on('click',function(){
+        
+        if(check==false)
+        {
+            $('#box').addClass('show');
+            check=true;
+        }
+        else{
+            $('#box').removeClass('show');
+            check=false;
+        }
+        
+    })
+
+    $('.submission').on('click',function(){
+        $('#box').removeClass('show');
+    })
+    $('.closeBtn').on('click',function(){
+        $('#box').removeClass('show');
+
+    })
+    
+});
